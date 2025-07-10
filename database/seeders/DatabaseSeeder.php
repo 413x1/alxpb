f226f500dc1da0b87c6c1419865825d494db2e7b
alexz11 <msholehz0111@gmail.com>
2025-07-10 21:12:04 +0700

    add seeder voucher

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(DeviceSeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(ProductBannerSeeder::class);
+        $this->call(VoucherSeeder::class);
     }
 }
